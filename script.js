9ae8ce6c257784f6900d94132e25a6b5bbafa950
Shruthi-10 <150574359+Shruthi-10@users.noreply.github.com>
2024-04-21 17:13:49 -0700

    First commit of Minimalist todo list application

---
+++ script.js
@@ -0,0 +1,6 @@
+const addGuestButton = document.querySelector("button");
+
+addGuestButton.addEventListener("click", function () {
+    const taskList = document.createElement("li");
+    console.log(taskList);
+};
